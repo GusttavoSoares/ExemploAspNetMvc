ec42eee374d7d609ece3c818a1db63dd611021fd
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-08-16 15:22:42 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAspNetMvc.Models;
+
+namespace ExemploAspNetMvc.Controllers;
+
+public class UserRequest 
+{
+    public string Nome { get; set; }
+    public String Email { get; set; }
+}
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult PrimeiraAction() 
+    {
+        return View();
+    }
+
+    public string TesteQueryString([FromQuery] string q, [FromQuery] string nome)
+    {
+        return $"Chegou aqui {q} e {nome}";
+    }
+
+    // public string TesteFormData([FromForm] string nome, [FromForm] string email)
+    // {
+    //     return $"Nome: {nome}, E-mail: {email}";
+    // }
+
+    public IActionResult Formulario() 
+    {
+        return View();
+    }
+    
+    public string TesteFormData([FromForm] UserRequest userRequest, [FromHeader] string x)
+    {
+        return $"Nome: {userRequest.Nome}, E-mail: {userRequest.Email}, x: {x}";
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
