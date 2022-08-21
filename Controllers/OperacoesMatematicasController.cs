e38c8d4887c39722216a1b0d4647facc89b3e68a
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-08-21 15:12:37 -0300

    feat: add results views for math operations

--- Controllers/OperacoesMatematicasController.cs
+++ Controllers/OperacoesMatematicasController.cs
@@ -4,6 +4,16 @@ using ExemploAspNetMvc.Models;
 
 namespace ExemploAspNetMvc.Controllers;
 
+public class Number 
+{
+    public float Number1 { get; set; }
+    public float Number2 { get; set; }
+}
+
+public class OperacoesMatematicas {
+
+}
+
 public class OperacoesMatematicasController : Controller
 {
     private readonly ILogger<OperacoesMatematicasController> _logger;
@@ -23,9 +33,19 @@ public class OperacoesMatematicasController : Controller
         return View();
     }
 
+    public IActionResult ResultadoSoma([FromForm] Number number)
+    {
+        return View(number);
+    }
+
     public IActionResult Subtrair()
     {
         return View();
+    }
+
+    public IActionResult ResultadoSubtracao([FromForm] Number number)
+    {
+        return View(number);
     }
 
      public IActionResult Multiplicar()
@@ -33,9 +53,19 @@ public class OperacoesMatematicasController : Controller
         return View();
     }
 
+    public IActionResult ResultadoMultiplicacao([FromForm] Number number)
+    {
+        return View(number);
+    }
+
      public IActionResult Dividir()
     {
         return View();
+    }
+
+    public IActionResult ResultadoDivisao([FromForm] Number number)
+    {
+        return View(number);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
